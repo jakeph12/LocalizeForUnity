cefe41fef6491228980ca2cbf3fabd2e207ef2bc
jakeph12 <59140785+jakeph12@users.noreply.github.com>
2024-01-11 17:26:37 +0100

    Update LoadDictionarys.cs

--- Scripts/LoadDictionarys.cs
+++ Scripts/LoadDictionarys.cs
@@ -9,6 +9,7 @@ public enum Lagnguage
 }
 public class LoadDictionarys : MonoBehaviour
 {
+        public const string m_strSaveName = "Language";
     private static Dictionary<string, string> m_dcAllWord = new Dictionary<string, string>();
     private static Lagnguage m_enSavCh;
     public static Lagnguage m_enChangebl
@@ -17,10 +18,22 @@ public class LoadDictionarys : MonoBehaviour
         set
         {
             m_enSavCh = value;
-            PlayerPrefs.SetInt("Language", (int)m_enSavCh);
         }
     }
-    public static bool m_bInit;
+    private static bool _m_bInit;
+    public static bool m_bInit
+    {
+        get
+        {
+            if (!_m_bInit) Init();
+            return _m_bInit;
+        }
+        set
+        {
+            _m_bInit = value;
+            if (!_m_bInit) m_dcAllWord.Clear();
+        }
+    }
 
     private static bool Init()
     {
@@ -30,23 +43,24 @@ public class LoadDictionarys : MonoBehaviour
             Debug.LogError("You dont have localize File");
             return false;
         }
-        m_enSavCh = (Lagnguage)PlayerPrefs.GetInt("Language");
-        if(lcst.m_lLanguage.Count > 0)
-            foreach(Word lg in lcst.m_lLanguage[(int)m_enChangebl].AllWards)
+        m_enSavCh = (Lagnguage)PlayerPrefs.GetInt(m_strSaveName);
+        if (lcst.m_lLanguage.Count > 0)
+        {
+            m_dcAllWord.Clear();
+            foreach(Word lg in lcst.m_lLanguage[(int)m_enChangebl].m_lisAllWards)
             {
-                m_dcAllWord.Add(lg.Key, lg.Value);
+                m_dcAllWord.Add(lg.m_strKey, lg.m_strValue);
             }
-        m_bInit = true;
+            m_bInit = true;
+        }
         return true;
     }
     public static string GetValue(string Key)
     {
-        if (!Init()) return Key;
+        if (!m_bInit) return Key;
         string value;
         m_dcAllWord.TryGetValue(Key,out value);
         value ??= Key;
-
-        
         return value;
     }
 }
