b83f732bb113f04329ab07ee9b642c91987036fd
jakeph12 <59140785+jakeph12@users.noreply.github.com>
2024-01-11 17:25:16 +0100

    Update LocalizeSettings.cs

--- Scripts/LocalizeSettings.cs
+++ Scripts/LocalizeSettings.cs
@@ -1,29 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using UnityEditor.Callbacks;
 
 [CreateAssetMenu(fileName = "New Localize File", menuName = "Setting/New Localize File")]
 public class LocalizeSettings : ScriptableObject
 {
-    public List<Languages> m_lLanguage = new List< Languages>();
+    public List<Languages> m_lLanguage = new List<Languages>();
 }
 
 [System.Serializable]
 public class Languages
 {
-    public string NameLanguage; 
-    public List<Word> AllWards = new List<Word>();
+    public string m_strNameLanguage; 
+    public List<Word> m_lisAllWards = new List<Word>();
 }
 [System.Serializable]
 
 public class Word
 {
-    public string Key, Value;
+    public string m_strKey, m_strValue;
     public Word(string key, string value)
     {
-        Key = key;
-        Value = value;
+        m_strKey = key;
+        m_strValue = value;
     }
 }
