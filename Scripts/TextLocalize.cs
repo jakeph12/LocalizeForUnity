58d103fe34157fdd489292d924c4596bcc3c2b97
jakeph12 <savrasovvitya@gmail.com>
2025-07-02 12:51:15 +0200

    Bug fix

--- Scripts/TextLocalize.cs
+++ Scripts/TextLocalize.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextLocalize : MonoBehaviour
 {
@@ -14,12 +15,12 @@ public class TextLocalize : MonoBehaviour
     void Start()
     {
         if (m_strKey == null || m_strKey == "" || m_strKey == " ") return;
-        if (m_enTypeOfLocalize == TypeOfLocalize.Word) GetComponent<Text>().text = LoadTranslate.GetValue(m_strKey);
+        if (m_enTypeOfLocalize == TypeOfLocalize.Word) GetComponent<Text>().text = LoadDictionarys.GetValue(m_strKey);
         else TranlateDialog();
     }
     void TranlateDialog()
     {
-        string d = LoadTranslate.GetValue(m_strKey);
+        string d = LoadDictionarys.GetValue(m_strKey);
         string[] dp = d.Split('|');
         var t = GetComponent<Text>();
         t.text = dp[0];
